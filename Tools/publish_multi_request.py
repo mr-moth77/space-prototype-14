867544f89aa8bfdfadd27eca63da88e8fcba2002
Argonaut <154510884+Arrgonat6773@users.noreply.github.com>
2026-06-02 02:01:46 +0500

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -16,7 +16,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.station14.ru/"
+ROBUST_CDN_URL = "http://89.22.237.25:5000/"
 
 def main():
     parser = argparse.ArgumentParser()
